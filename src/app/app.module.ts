88c8823b4d706d59d08dec93e953742f69e6bbe5
davidg28 <davidgonzalez28@protonmail.com>
2021-01-06 06:33:02 +0400

    Add service worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { CartComponent } from './components/cart/cart.component';
 import { ShippingComponent } from './components/shipping/shipping.component';
 import { MissionControlComponent } from './components/missioncontrol/missioncontrol.component';
 import { AstronautComponent } from './components/astronaut/astronaut.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   imports: [
@@ -25,7 +27,8 @@ import { AstronautComponent } from './components/astronaut/astronaut.component';
       { path: 'cart', component: CartComponent },
       { path: 'shipping', component: ShippingComponent },
       { path: 'mission', component: MissionControlComponent },
-    ])
+    ]),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   declarations: [
     AppComponent,
