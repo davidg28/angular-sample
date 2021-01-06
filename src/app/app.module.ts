d0446b69bf158b08d857a50be85d71c0bcba2a29
davidg28 <davidgonzalez28@protonmail.com>
2021-01-06 07:22:11 +0400

    Add animation feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -13,12 +14,14 @@ import { CartComponent } from './components/cart/cart.component';
 import { ShippingComponent } from './components/shipping/shipping.component';
 import { MissionControlComponent } from './components/missioncontrol/missioncontrol.component';
 import { AstronautComponent } from './components/astronaut/astronaut.component';
+import { AnimationComponent } from './components/animation/animation.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
@@ -27,6 +30,7 @@ import { environment } from '../environments/environment';
       { path: 'cart', component: CartComponent },
       { path: 'shipping', component: ShippingComponent },
       { path: 'mission', component: MissionControlComponent },
+      { path: 'animation', component: AnimationComponent },
     ]),
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
@@ -39,7 +43,8 @@ import { environment } from '../environments/environment';
     CartComponent,
     ShippingComponent,
     MissionControlComponent,
-    AstronautComponent
+    AstronautComponent,
+    AnimationComponent
   ],
   bootstrap: [
     AppComponent
---
+++ src/app/components/animation/animation.component.ts
@@ -0,0 +1,52 @@
+import { Component, Input } from '@angular/core';
+import { trigger, transition, state, animate, style, AnimationEvent } from '@angular/animations';
+
+@Component({
+  selector: 'app-animation',
+  animations: [
+    trigger('openClose', [
+      state('open', style({
+        height: '200px',
+        opacity: 1,
+        backgroundColor: 'yellow'
+      })),
+      state('closed', style({
+        height: '100px',
+        opacity: 0.5,
+        backgroundColor: 'green'
+      })),
+      transition('open => closed', [
+        animate('1s')
+      ]),
+      transition('closed => open', [
+        animate('0.5s')
+      ]),
+      transition('* => closed', [
+        animate('1s')
+      ]),
+      transition('* => open', [
+        animate('0.5s')
+      ]),
+      transition('open <=> closed', [
+        animate('0.5s')
+      ]),
+      transition ('* => open', [
+        animate ('1s',
+          style ({ opacity: '*' }),
+        ),
+      ]),
+      transition('* => *', [
+        animate('1s')
+      ]),
+    ]),
+  ],
+  templateUrl: 'animation.component.html',
+  styleUrls: ['animation.component.scss']
+})
+export class AnimationComponent {
+  isOpen = true;
+
+  toggle() {
+    this.isOpen = !this.isOpen;
+  }
+}
